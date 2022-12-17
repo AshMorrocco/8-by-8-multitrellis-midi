394a39c1b82b033a768a7d9d4b5463bb3406608a
Ashford Morrocco <AshMorrocco@users.noreply.github.com>
2022-12-16 20:52:49 -0500

    enhanced comments, seperating callbacks

--- MidiController.py
+++ MidiController.py
@@ -4,7 +4,11 @@ from adafruit_neotrellis.multitrellis import MultiTrellis
 # Helper class for shorthand midi values
 class MidiNotes:
     def __init__(self):
+        # 60 is Middle C | C4
+        # Gives us a way to access note with index 0-11
         self.note_list_chromatic = [60,61,62,63,64,65,66,67,68,69,70,71]
+
+        # Note offset from root of the 7 major western modes
         self.scale_ionian =      [0,2,4,5,7,9,11,128]
         self.scale_dorian =      [0,2,3,5,7,8,10,128]
         self.scale_phrygian =    [0,1,3,5,7,8,10,128]
@@ -12,7 +16,11 @@ class MidiNotes:
         self.scale_mixolydian =  [0,2,4,5,7,9,10,128]
         self.scale_aeolian =     [0,2,3,5,7,8,10,128]
         self.scale_locrian =     [0,1,3,5,6,8,10,128]
+
+        # Lets us reference the scales 0-6
         self.scale_array = [self.scale_ionian,self.scale_dorian,self.scale_phrygian,self.scale_lydian,self.scale_mixolydian,self.scale_aeolian,self.scale_locrian]
+        
+        # Shorthand for major and minor scales
         self.scale_major = self.scale_ionian
         self.scale_minor = self.scale_aeolian
 
@@ -21,24 +29,27 @@ class MidiController(MultiTrellis, MidiNotes):
     def __init__(self, neotrellis_array):
         MultiTrellis.__init__(self, neotrellis_array)
         MidiNotes.__init__(self)
-        self.scale = self.scale_array[0]
-        self.rootnote = self.note_list_chromatic[0]
+
+        # Default Instance Values
+        self.current_scale_mode = self.scale_array[0]
+        self.current_rootnote = self.note_list_chromatic[0]
         self.velocity = 127
         self.noteoffset = 60
 
+    # Honestly this belongs elsewhere, I just like random colors
     def random_0_255_tuple(self):
         randtuple = (random.randint(0, 255),random.randint(0, 255),random.randint(0, 255))
         return randtuple
 
-    def getScale(self):
-        return self.scale
+    def getScaleMode(self):
+        return self.current_scale_mode
 
-    def setScale(self, newScale):
+    def setScaleMode(self, newScale):
         print("Setting Scale to "+str(newScale))
-        self.scale = newScale
+        self.current_scale_mode = newScale
 
     def button_to_note(self, x,y):
-        current_scale = self.getScale() 
+        current_scale = self.getScaleMode() 
         #choose the root note and scale
         note = self.noteoffset + current_scale[x]    
         octave = (y-2)*12
--- main.py
+++ main.py
@@ -1,23 +1,13 @@
+# Designed for Adafruit 8x8 Neotrellis with M4 Express Feather
 from board import SCL, SDA
 import busio
 import adafruit_midi
 import usb_midi
 
 from adafruit_midi.note_on          import NoteOn
+from adafruit_midi.note_off          import NoteOff
 from adafruit_neotrellis.neotrellis import NeoTrellis
 from MidiController import MidiController
-
-# create the i2c object for the trellis
-i2c_bus = busio.I2C(SCL, SDA)
-
-#access for one board
-board1 = NeoTrellis(i2c_bus, False, addr=0x30)
-
-#set addr for each neotrellis, False is for inturrupt pin disable
-trelli = [
-    [NeoTrellis(i2c_bus, False, addr=0x30), NeoTrellis(i2c_bus, False, addr=0x2F)],
-    [NeoTrellis(i2c_bus, False, addr=0x31), NeoTrellis(i2c_bus, False, addr=0x2E)],
-]
 
 # some color definitions
 OFF = (0, 0, 0)
@@ -28,44 +18,70 @@ CYAN = (0, 255, 255)
 BLUE = (0, 0, 255)
 PURPLE = (180, 0, 255)
 
+# create the i2c object for the trellis
+i2c_bus = busio.I2C(SCL, SDA)
 
+# UPDATE I2C ADDRESS TO MATCH BOARD
+# set addr for each neotrellis, False disables inturrupt pin, which is not used.
+trelli = [
+    [NeoTrellis(i2c_bus, False, addr=0x30), NeoTrellis(i2c_bus, False, addr=0x2F)],
+    [NeoTrellis(i2c_bus, False, addr=0x31), NeoTrellis(i2c_bus, False, addr=0x2E)],
+]
+
+# midi is the interface that sends to connected device
 midi = adafruit_midi.MIDI(midi_out=usb_midi.ports[1])
-    
+
+# midicontroller refers to the 8x8 Multitrellis    
 midicontroller = MidiController(trelli)
 
- # this will be called when button events are received
-def on_button_press(x, y, edge):
-    #Button Pressed
+# The meat and potatoes.
+# Runs whenever a button is pressed or released, 
+# x,y is 0-indexed position of button in midicontroller
+# edge = EDGE_RISING on button press
+# edge = EDGE_FALLING on button release
+def start_stop_note(x, y, edge):
     if edge == NeoTrellis.EDGE_RISING:
         midicontroller.color(x, y, (255, 0, 0))
-        print(str(midicontroller.button_to_note(x,y))+" "+str(midicontroller.velocity))
         midi.send(NoteOn(midicontroller.button_to_note(x,y)))
-        if x == 7:
-            if y >= len(midicontroller.scale_array):
-                return
-            midicontroller.setScale(midicontroller.scale_array[y])
-
-    # Button Released
-    elif edge == NeoTrellis.EDGE_FALLING:
-        midi.send(NoteOn(midicontroller.button_to_note(x,y), 0))
+        
+    else:
+        midi.send(NoteOff(midicontroller.button_to_note(x,y), 0))
         midicontroller.color(x, y, (0,0,0))
 
+def change_scale_mode(x, y, edge):
+    if edge == NeoTrellis.EDGE_RISING:
+        if y >= len(midicontroller.scale_array):
+                return
+        midicontroller.setScaleMode(midicontroller.scale_array[y])
+
+
+# Boot sequence ~~~ 
 # Enable Callbacks
 for y in range(8):
     for x in range(8):
         # activate rising edge events on all keys
         midicontroller.activate_key(x, y, NeoTrellis.EDGE_RISING, True)
+
         # activate falling edge events on all keys
         midicontroller.activate_key(x, y, NeoTrellis.EDGE_FALLING, True)
-        midicontroller.set_callback(x, y, on_button_press)
-        midicontroller.color(x, y, midicontroller.random_0_255_tuple())
-        #time.sleep(0.05)
 
+        # Rightmost column is dedicated to mode selection
+        if x == 7:
+            midicontroller.set_callback(x, y, change_scale_mode)
+        else:
+            midicontroller.set_callback(x, y, start_stop_note)
+        
+        midicontroller.color(x, y, midicontroller.random_0_255_tuple())
+
+        # Uncomment to have more of a startup animation
+        # time.sleep(0.05)
+
+# Turn off the pretty lights to finish boot sequence
 for y in range(8):
     for x in range(8):
         midicontroller.color(x, y, OFF)
-        #time.sleep(0.05)
 
+# Infinite loop of music
 while True:
     midicontroller.sync()
 
